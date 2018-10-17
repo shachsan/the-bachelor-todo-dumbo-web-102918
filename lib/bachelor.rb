6dd53f5a6ddffbe1e577c2202160c05cf89c700a
SANJAY K SAI <shachsan@hotmail.com>
2018-10-17 03:29:08 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -63,7 +63,7 @@ def get_occupation(data, hometown)
   contestant_bio_data(data, hometown).each do |bio_data_hash|
     occupations << bio_data_hash["occupation"]
   end
-  return occupations[0].join(", ")
+  return occupations.first
 end
 
 def get_average_age_for_season(data, season)
